073214380fa62198f5db6f1182e9964fec0662e7
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-10-26 02:33:07 +0300

    Minor updates

--- src/todos-control.js
+++ src/todos-control.js
@@ -1,8 +1,7 @@
-import * as L                      from "partial.lenses"
-import * as R                      from "ramda"
-import Atom                        from "kefir.atom"
-import K, {bind, classes, fromIds} from "karet.util"
-import React                       from "karet"
+import * as L    from "partial.lenses"
+import * as R    from "ramda"
+import K, * as U from "karet.util"
+import React     from "karet"
 
 import {hash} from "./window"
 
@@ -15,34 +14,40 @@ const routes =
 
 const route = K(hash, h => routes.find(r => r.hash === h) || routes[0])
 
-const Todo = ({todo, editing = Atom(false)}) =>
-  <li {...classes(K(todo, todo =>
-                    L.get(M.Todo.completed, todo) && "completed"),
-                  K(editing, "editing", R.and))}>
+const Entry = ({editing, todo}) => {
+  const focus = e => {
+    e.focus()
+    e.selectionStart = e.value.length
+  }
+  const exit = () => editing.set("")
+  const save = e => {
+    const newTitle = e.target.value.trim()
+    exit()
+    newTitle ? todo.view(M.Todo.title).set(newTitle) : todo.remove()
+  }
+  return <input className="edit"
+                type="text"
+                onBlur={save}
+                key="x"
+                ref={c => c && focus(c)}
+                defaultValue={todo.view(M.Todo.title)}
+                onKeyDown={e => e.key === "Enter"  && save(e)
+                             || e.key === "Escape" && exit()}/>
+}
+
+const Todo = ({todo, editing = U.atom("")}) =>
+  <li {...U.classes(K(todo, todo =>
+                      L.get(M.Todo.completed, todo) && "completed"),
+                    editing)}>
     <input className="toggle"
            type="checkbox"
            hidden={editing}
-           {...bind({checked: todo.lens(M.Todo.completed)})}/>
-    <label className="view" onDoubleClick={() => editing.set(true)}>
+           {...U.bind({checked: todo.view(M.Todo.completed)})}/>
+    <label className="view" onDoubleClick={() => editing.set("editing")}>
       {todo.view(M.Todo.title)}
     </label>
     <button className="destroy" onClick={() => todo.remove()}/>
-    {K(editing, e => e && (() => {
-      const focus = e => {e.focus(); e.selectionStart = e.value.length}
-      const exit = () => editing.set(false)
-      const save = e =>
-        {const newTitle = e.target.value.trim()
-         exit()
-         newTitle ? todo.lens(M.Todo.title).set(newTitle)
-                  : todo.remove()}
-      return <input className="edit"
-                    type="text"
-                    onBlur={save}
-                    key="x"
-                    ref={c => c && focus(c)}
-                    defaultValue={todo.view(M.Todo.title)}
-                    onKeyDown={e => e.key === "Enter"  && save(e)
-                                 || e.key === "Escape" && exit()}/>})())}
+    {U.when(editing, () => <Entry {...{editing, todo}}/>)}
   </li>
 
 const NewTodo = ({onEntry}) =>
@@ -50,9 +55,9 @@ const NewTodo = ({onEntry}) =>
          type="text"
          autoFocus
          placeholder="What needs to be done?"
-         onKeyDown={({which, target}) => {
+         onKeyDown={({key, target}) => {
            const title = target.value.trim()
-           if (which === 13 && title !== "") {
+           if (key === "Enter" && title) {
              onEntry(title)
              target.value = ""
            }
@@ -62,14 +67,14 @@ const Filters = () =>
   <ul className="filters">
     {routes.map(r =>
       <li key={r.title}>
-        <a {...classes(K(route, c => c.hash === r.hash && "selected"))}
+        <a {...U.classes(K(route, c => c.hash === r.hash && "selected"))}
            href={r.hash}>
           {r.title}
         </a>
       </li>)}
   </ul>
 
-export default ({todos, all = todos.lens(M.Todos.all)}) =>
+export default ({todos, all = todos.view(M.Todos.all)}) =>
   <div>
     <section className="todoapp">
       <header className="header">
@@ -81,11 +86,11 @@ export default ({todos, all = todos.lens(M.Todos.all)}) =>
         <input type="checkbox"
                className="toggle-all"
                hidden={K(all, M.All.isEmpty)}
-               {...bind({checked: all.lens(M.All.allDone)})}/>
+               {...U.bind({checked: all.view(M.All.allDone)})}/>
         <ul className="todo-list">
-          {fromIds(K(route, all, ({filter}, all) =>
-                     R.flatten(all.map((it, i) => filter(it) ? [i] : []))),
-                   i => <Todo key={i} todo={all.lens(i)}/>)}
+          {U.seq(K(route, all, ({filter}, all) =>
+                   R.flatten(all.map((it, i) => filter(it) ? [i] : []))),
+                 U.mapCached(i => <Todo key={i} todo={all.view(i)}/>))}
         </ul>
       </section>
       <footer className="footer" hidden={K(all, M.All.isEmpty)}>
